7a621c7c71c85623c687c16c4f6751422f35f7af
Arkadij Konstantinov <ntbserviceweb@gmail.com>
2026-05-20 19:55:14 +0200

    Добавление CI/CD и Github Actions

---
+++ Lesson_18/conftest.py
@@ -0,0 +1,40 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture
+def driver():
+    # print("🌐 Запускаем браузер...")
+    #
+    # chrome_options = Options()
+    # # # Словарь настроек для отключения уведомлений
+    # # prefs = {
+    # #     "credentials_enable_service": False,  # Отключаем предложение сохранить пароль
+    # #     "profile.password_manager_enabled": False,  # Отключаем сам менеджер паролей
+    # #     "profile.password_manager_leak_detection": False,  # Отключаем проверку утечек паролей
+    # #     "safebrowsing.enabled": False  # Отключаем антивирус (Safe Browsing)
+    # # }
+    # #
+    # # chrome_options.add_experimental_option("prefs", prefs)
+    # # chrome_options.add_argument("--disable-features=PasswordLeakDetection")  # Отключаем функцию проверки утечек паролей
+    # #
+    # # # Отключаем табличку "Chrome управляется тестовым ПО"
+    # # chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+
+    options = Options()
+    options.add_argument('--headless=new') #Режим без визуального отбражения
+    options.add_argument('--window-size=1920,1080') #Установка размера окна для корректной работы в headless режиме
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    service = Service(ChromeDriverManager().install())
+    my_driver = webdriver.Chrome(service=service, options=options)
+
+
+    yield my_driver
+
+
+    my_driver.quit()
